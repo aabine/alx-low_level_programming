7934dc6da936e7d72c3068d8175f688380c6872b
aabine <kendoaustine@yahoo.com>
2023-03-17 00:29:48 +0000

    ALX

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /*
-This is a multi-line comment
-that provides a detailed explanation
-of the following code.
+*This is a multi-line comment
+*that provides a detailed explanation
+*of the following code.
 */
-
 int main(void)
 {
+	/*This will print to the terminal*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
