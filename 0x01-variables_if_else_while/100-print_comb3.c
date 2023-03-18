fe595476668ded202471d88bd633b0c2ed1de21e
aabine <kendoaustine@yahoo.com>
2023-03-18 23:25:19 +0000

    ALX

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,20 +8,21 @@
 
 int main(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 1; i < 90; i++)
+	for (i = 0; i < 9; i++)
 	{
-		putchar(i / 10 + '0');
-		putchar(i % 10 + '0');
-		if (!(i == 89))
+		for (j = i + 1; j <= 9; j++)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(i + '0');
+			putchar(j + '0');
+			if (!(i == 8 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
-	
 	return (0);
-
 }
