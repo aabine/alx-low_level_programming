9565845b541c01d2b1321f2ad3ecb5e9120aaf31
aabine <kendoaustine@yahoo.com>
2023-03-18 02:26:04 +0000

    ALX

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 25; i >= 0; i--)
+	{
+		putchar('a' + i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
