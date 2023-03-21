4a8d2a4c1620bdd9c49d6010a449d8f8838beb7c
aabine <kendoaustine@yahoo.com>
2023-03-21 23:08:54 +0000

    ALX this is a function that prints the alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - prints the alphabet in lowercase
  *
- * Return: Always 0 (success)
+ * Return: void.
  */
 
 void print_alphabet(void);
