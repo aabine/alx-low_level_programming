d377095e33d34242cb3e28cdba1436c949351dd4
aabine <kendoaustine@yahoo.com>
2023-03-22 04:42:07 +0000

    ALX

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: sum of a and b
+ */
+
+int add(int x, int y);
+
+int add(int x, int y)
+{
+	return (x + y);
+}
