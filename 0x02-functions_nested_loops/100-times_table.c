9be9d3ba5d8526a596d5dc789cc46deea19581d7
aabine <kendoaustine@yahoo.com>
2023-03-23 06:22:50 +0000

    ALX

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,41 +8,44 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, k;
 
-	if (n > 0 && n < 15)
+
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				int c = i * j;
-
+				k = j * i;
 				if (j == 0)
 				{
-					_putchar(' ');
-				}
-				_putchar(' ');
-				if (c < 10)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(c + '0');
-				}
-				else if (c < 100)
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
+					_putchar(',');
 					_putchar(' ');
-					_putchar(c / 10 + '0');
-					_putchar(c % 10 + '0');
-				}
-				else
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
 				{
-					_putchar(c / 100 + '0');
-					_putchar((c % 100) / 10 + '0');
-					_putchar(c % 10 + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
 		}
 	}
 }
+
