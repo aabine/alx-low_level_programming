7065183952ea698d71d5cc2bf118e029591254da
aabine <kendoaustine@yahoo.com>
2023-03-23 23:19:48 +0000

    AUSTINE

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - this function will multiply the user input
+ * @a: the input to multiply
+ * @b: the other input to multiply
+ *
+ * Return: the return type is an integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
