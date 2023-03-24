d6f867a470b9c0e43840e5a7bdf8355423836f69
aabine <kendoaustine@yahoo.com>
2023-03-24 02:48:27 +0000

    Austine

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,22 @@
 
 void print_diagonal(int n)
 {
-	int i,j;
-
-	for ( i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for ( j = 0; j < i; j++)
-		{
-			if (n <= 0)
-				_putchar('\n');
-			_putchar(' ');
-		}
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
