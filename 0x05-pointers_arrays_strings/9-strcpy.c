171c6baa03a667541b2c56a12cc5e49f2a0f2e75
aabine <kendoaustine@yahoo.com>
2023-03-28 03:47:13 +0000

    Austine

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string from one varaiable to another
+ * @dest: the destination buffer
+ * @src: the source string.
+ *
+ * Return: a pointer to the destinaton buffer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+	return start;
+}
