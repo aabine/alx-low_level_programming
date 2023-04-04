a781c98b43c2308f4551d820f38985e8aab0ded5
aabine <kendoaustine@yahoo.com>
2023-04-04 01:22:03 +0000

    Austine

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,14 +14,17 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (c == s[i])
+		if (*s == c)
 		{
-			return (s + i);
+			return (s);
 		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
 	}
 	return (NULL);
 }
