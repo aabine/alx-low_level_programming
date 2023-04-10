78fc10f9074dd4428628b31ccdaec0d48ad1938f
aabine <kendoaustine@yahoo.com>
2023-04-10 03:36:44 +0000

    Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	for (counter = 0; counter < argc; counter++)
 	{
-		printf("This is the name of these function: %s\n", argv[counter]);
+		printf("%s\n", argv[counter]);
 	}
 
 	return (0);
