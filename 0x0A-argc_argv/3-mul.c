575a05ed508340db5ae3d6adcf4eb7fe54e9cf47
aabine <kendoaustine@yahoo.com>
2023-04-10 04:15:24 +0000

    Write a program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point. Multiplies two numbers from the command line.
+ *
+ * @argc: The number of command line arguments.
+ * @argv: An array of pointers to the command line arguments.
+ *
+ * Return: 0 if successful, otherwise 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	num1 = atoll(argv[1]);
+	num2 = atoll(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
