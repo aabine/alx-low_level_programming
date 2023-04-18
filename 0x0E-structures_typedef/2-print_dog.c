6684458e69c3d697b8256edc2a6069abba8b2a6c
aabine <kendoaustine@yahoo.com>
2023-04-18 04:36:10 +0000

    ALX

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,13 +5,15 @@
  * print_dog - prints the information of a dog
  * @d: pointer to a dog struct
  *
- * Description: This function prints the information of a dog, including its name,
+ * Description: This function prints the information of a dog,
+ * including its name,
  * age, and owner.
  */
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
+		return;
 	{
 		printf("Name: %s\n", d->name);
 		printf("Age: %1f\n", d->age);
