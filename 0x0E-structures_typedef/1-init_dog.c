757f4dd7fbd1ee65469dfeac801825dc98cee567
aabine <kendoaustine@yahoo.com>
2023-04-18 04:31:57 +0000

    ALX

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	/*Check if the pointer to the dog struct is not NULL*/
-	if (d != NULL) {
+	if (d != NULL)
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
