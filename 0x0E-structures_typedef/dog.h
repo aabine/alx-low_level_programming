b0812a5b4e182c1aa908652656821a290bbd09ee
aabine <kendoaustine@yahoo.com>
2023-04-18 04:30:03 +0000

    ALX

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,16 @@
  * Description: structure that stores information about a dog
  */
 
-struct dog{
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
