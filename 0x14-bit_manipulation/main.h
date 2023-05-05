3489358c5ebbea5d68579b14effa28066488448a
aabine <kendoaustine@yahoo.com>
2023-05-05 22:10:33 +0100

    ALX

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,7 @@
 #include <limits.h>
 #include <string.h>
 
-
+int get_endianness(void);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
