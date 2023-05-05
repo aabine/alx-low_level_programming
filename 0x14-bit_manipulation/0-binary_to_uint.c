f76767bc0ce364087c9595109e92f6e6264cce38
aabine <kendoaustine@yahoo.com>
2023-05-05 20:25:33 +0100

    ALX

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,23 @@
  *
  * Return: the converted unsigned int, or 0 if there is an error
  */
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int decimal = 0;
-	int i;
 
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] == '1')
-			decimal = (decimal << 1) | 1;
-		else if (b[i] == '0')
-			decimal <<= 1;
-		else
-			return (NULL);
-	}
+unsigned int binary_to_uint(const char *b) 
+{ 
+	unsigned int decimal = 0; 
+	size_t i; 
 
-	return (decimal);
+	for (i = 0; i < strlen(b); i++) { 
+		if (b[i] == '1') 
+			decimal = (decimal << 1) | 1; 
+		else if (b[i] == '0') 
+			decimal <<= 1; 
+		else 
+			return (0); 
+	} 
+
+	if (i > strlen(b)) 
+		return (-1); 
+
+	return (decimal); 
 }
