08bcecb6286a6456b809857aaf87b2b644476a30
aabine <kendoaustine@yahoo.com>
2023-05-12 05:23:56 +0100

    ALX

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,19 +1,10 @@
 #include "main.h"
-#include <stdbool.h>
 
-/**
- * main - Entry point
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success, 97-100 on failure
- */
 int main(int argc, char *argv[])
 {
 	int fd;
-	struct elf_header elf_header;
-	bool error = false;
-
+	unsigned char header[52];
+	struct elf_header elf_header = {0};
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: elf_header elf_filename\n");
@@ -25,37 +16,147 @@ int main(int argc, char *argv[])
 	if (fd == -1)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		error = true;
+		exit(98);
 	}
 
-	if (!error && read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
+	if (!read_elf_header(fd, header))
 	{
 		fprintf(stderr, "Error: Can't read ELF header from file %s\n", argv[1]);
-		error = true;
-	}
-
-	if (!error && elf_header.magic != 0x7F454C46)
-	{
-		fprintf(stderr, "Error: Not an ELF file\n");
-		error = true;
-	}
-
-	if (error)
-	{
 		close(fd);
 		exit(98);
 	}
 
-	printf("Magic: 0x%08x\n", elf_header.magic);
-	printf("Class: 0x%02x\n", elf_header.class);
-	printf("Data: 0x%02x\n", elf_header.data);
-	printf("Version: 0x%02x\n", elf_header.version);
-	printf("OS/ABI: 0x%02x\n", elf_header.osabi);
-	printf("ABI Version: 0x%02x\n", elf_header.abiversion);
-	printf("Type: 0x%02x\n", elf_header.type);
-	printf("Entry point address: 0x%08x\n", elf_header.entry);
+	if (!is_elf_file(header))
+	{
+		fprintf(stderr, "Error: Not an ELF file\n");
+		close(fd);
+		exit(98);
+	}
+
+	parse_elf_header(fd, &elf_header);
+	print_elf_header(elf_header);
+	free_fields(elf_header.magic);
+	free_fields(elf_header.class);
+	free_fields(elf_header.data);
+	free_fields(elf_header.version);
+	free_fields(elf_header.osabi);
+	free_fields(elf_header.abiversion);
+	free_fields(elf_header.pad);
+	free_fields(elf_header.type);
+	free_fields(elf_header.machine);
+	free_fields(elf_header.version2);
+	free_fields(elf_header.entry);
+	free_fields(elf_header.phoff);
+	free_fields(elf_header.shoff);
+	free_fields(elf_header.flags);
+	free_fields(elf_header.ehsize);
+	free_fields(elf_header.phentsize);
+	free_fields(elf_header.phnum);
+	free_fields(elf_header.shentsize);
+	free_fields(elf_header.shnum);
+	free_fields(elf_header.shstrndx);
 
 	close(fd);
 
 	return (0);
+}
+
+void append_field(struct elf_header_field **head, char *name, int size, unsigned char *data)
+{
+	struct elf_header_field *new_field = (struct elf_header_field *)malloc(sizeof(struct elf_header_field));
+	new_field->name = name;
+	new_field->size = size;
+	new_field->data = data;
+	new_field->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_field;
+	}
+	else
+	{
+		struct elf_header_field *current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_field;
+	}
+}
+
+void free_fields(struct elf_header_field *head)
+{
+	while (head != NULL)
+	{
+		struct elf_header_field *current = head;
+		head = head->next;
+		free(current->data);
+		free(current);
+	}
+}
+
+bool read_elf_header(int fd, unsigned char *header)
+{
+	if (read(fd, header, sizeof(unsigned char) * 52) != sizeof(unsigned char) * 52)
+	{
+		return false;
+	}
+	return true;
+}
+
+bool is_elf_file(unsigned char *header)
+{
+	if (header[0] == 0x7f && header[1] == 'E' && header[2] == 'L' && header[3] == 'F')
+	{
+		return true;
+	}
+	return false;
+}
+
+void parse_elf_header(int fd, struct elf_header *elf_header)
+{
+	unsigned char header[52];
+	read_elf_header(fd, header);
+
+	append_field(&elf_header->magic, "Magic", 4, &header[0]);
+	append_field(&elf_header->class, "Class", 1, &header[4]);
+	append_field(&elf_header->data, "Data", 1, &header[5]);
+	append_field(&elf_header->version, "Version", 1, &header[6]);
+	append_field(&elf_header->osabi, "OS/ABI", 1, &header[7]);
+	append_field(&elf_header->abiversion, "ABI Version", 1, &header[8]);
+	append_field(&elf_header->pad, "Padding", 7, &header[9]);
+	append_field(&elf_header->type, "Type", 2, &header[16]);
+	append_field(&elf_header->machine, "Machine", 2, &header[18]);
+	append_field(&elf_header->version2, "Version2", 4, &header[20]);
+	append_field(&elf_header->entry, "Entry point address", 4, &header[24]);
+	append_field(&elf_header->phoff, "Program header table offset", 4, &header[28]);
+	append_field(&elf_header->shoff, "Section header table offset", 4, &header[32]);
+	append_field(&elf_header->flags, "Flags", 4, &header[36]);
+	append_field(&elf_header->ehsize, "ELF header size", 2, &header[40]);
+	append_field(&elf_header->phentsize, "Program header table entry size", 2, &header[42]);
+	append_field(&elf_header->phnum, "Program header table entry count", 2, &header[44]);
+	append_field(&elf_header->shentsize, "Section header table entry size", 2, &header[46]);
+	append_field(&elf_header->shnum, "Section header table entry count", 2, &header[48]);
+	append_field(&elf_header->shstrndx, "Section header string table index", 2, &header[50]);
+}
+
+void print_elf_header(struct elf_header elf_header)
+{
+	int i;
+	struct elf_header_field *current;
+
+	printf("ELF header:\n");
+	printf("===========\n");
+
+	current = elf_header.magic;
+	while (current != NULL)
+	{
+		printf("%s: ", current->name);
+		for (i = 0; i < current->size; i++)
+		{
+			printf("%02x ", (int)current->data[i]);
+		}
+		printf("\n");
+		current = current->next;
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,33 +13,52 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <sys/sendfile.h>
+#include <stdbool.h>
 
 
 #define BUFFER_SIZE 4096
 
-struct elf_header {
-	unsigned int magic;
-	unsigned char class;
-	unsigned char data;
-	unsigned char version;
-	unsigned char osabi;
-	unsigned char abiversion;
-	unsigned char pad[7];
-	unsigned short type;
-	unsigned short machine;
-	unsigned int version2;
-	unsigned int entry;
-	unsigned int phoff;
-	unsigned int shoff;
-	unsigned int flags;
-	unsigned short ehsize;
-	unsigned short phentsize;
-	unsigned short phnum;
-	unsigned short shentsize;
-	unsigned short shnum;
-	unsigned short shstrndx;
+struct elf_header_field {
+    char *name;
+    int size;
+    unsigned char *data;
+    struct elf_header_field *next;
 };
+
+struct elf_header {
+    struct elf_header_field *magic;
+    struct elf_header_field *class;
+    struct elf_header_field *data;
+    struct elf_header_field *version;
+    struct elf_header_field *osabi;
+    struct elf_header_field *abiversion;
+    struct elf_header_field *pad;
+    struct elf_header_field *type;
+    struct elf_header_field *machine;
+    struct elf_header_field *version2;
+    struct elf_header_field *entry;
+    struct elf_header_field *phoff;
+    struct elf_header_field *shoff;
+    struct elf_header_field *flags;
+    struct elf_header_field *ehsize;
+    struct elf_header_field *phentsize;
+    struct elf_header_field *phnum;
+    struct elf_header_field *shentsize;
+    struct elf_header_field *shnum;
+    struct elf_header_field *shstrndx;
+};
+
+void append_field(struct elf_header_field **head, char *name, int size, unsigned char *data);
+
+void free_fields(struct elf_header_field *head);
+
+bool read_elf_header(int fd, unsigned char *header);
+
+bool is_elf_file(unsigned char *header);
+
+void parse_elf_header(int fd, struct elf_header *elf_header);
+
+void print_elf_header(struct elf_header elf_header);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
