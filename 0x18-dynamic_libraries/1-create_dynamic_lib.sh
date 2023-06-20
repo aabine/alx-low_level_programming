bf44cbb17161996905316df7c99648f3b5980b3c
aabine <kendoaustine@yahoo.com>
2023-06-20 04:21:36 +0100

    DYNAMIC LIBRARY

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -shared -fPIC *.c -o liball.so
-chmod 755 liball.so
+gcc -shared -fPIC *.c -o 100-operations.so
+chmod 755 100-operations.so
+export LD_PRELOAD=100-operations.so
---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,66 @@
+/* functions definitions for operations */
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+*/
+
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+*/
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: quotient of a and b
+*/
+
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * mod - returns the remainder of dividing two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of a and b
+*/
+
+int mod(int a, int b)
+{
+    return (a % b);
+}
