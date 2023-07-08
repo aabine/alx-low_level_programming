acb0c8405fa28cf7475daf0ba56816116f7fa86a
aabine <kendoaustine@yahoo.com>
2023-07-08 21:45:45 +0100

    Heap assignment correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *) key, ht->size);
 	node = ht->array[index];
 
-	while (node != NULL)
+	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
