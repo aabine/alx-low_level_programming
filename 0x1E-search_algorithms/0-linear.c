caad5eb6fd4fca033163618b75a575452d4cc964
aabine <kendoaustine@yahoo.com>
2023-10-12 03:35:31 +0100

    fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,11 +13,11 @@ int linear_search(int *array, size_t size, int value)
     if (array == NULL)
         return (-1);
 
-    size_t i;
+    int i;
 
-    for (i = 0; i < size; i++)
+    for (i = 0; i < (int)size; i++)
     {
-        printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+        printf("Value checked array[%u] = [%d]\n", i, array[i]);
 
         if (array[i] == value)
             return i;
