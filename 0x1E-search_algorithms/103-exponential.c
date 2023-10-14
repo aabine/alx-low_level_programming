081897ab404663b1ca25d692ecd14a6213639850
aabine <kendoaustine@yahoo.com>
2023-10-14 16:08:58 +0100

    fixed

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -12,31 +12,30 @@
  */
 int binary_search_recursive(int *array, int l_index, int r_index, int value)
 {
-	if (r_index >= l_index)
+	int i, mid_index;
+
+	if (r_index < l_index)
+		return -1;
+
+	mid_index = l_index + (r_index - l_index) / 2;
+
+	printf("Searching in array: ");
+	for (i = l_index; i <= r_index; i++)
 	{
-		int mid_index = l_index + (r_index - l_index) / 2;
-		int i;
-
-		printf("Searching in array: ");
-		for (i = l_index; i <= r_index; i++)
-		{
-			printf("%d", array[i]);
-			if (i == r_index)
-				printf("\n");
-			else
-				printf(", ");
-		}
-
-		if (array[mid_index] == value)
-			return (mid_index);
-
-		if (array[mid_index] > value)
-			return (binary_search_recursive(array, l_index, mid_index - 1, value));
-
-		return (binary_search_recursive(array, mid_index + 1, r_index, value));
+		printf("%d", array[i]);
+		if (i == r_index)
+			printf("\n");
+		else
+			printf(", ");
 	}
 
-	return (-1);
+	if (array[mid_index] == value)
+		return mid_index;
+
+	if (array[mid_index] > value)
+		return binary_search_recursive(array, l_index, mid_index - 1, value);
+
+	return binary_search_recursive(array, mid_index + 1, r_index, value);
 }
 
 /**
@@ -50,24 +49,28 @@ int binary_search_recursive(int *array, int l_index, int r_index, int value)
  */
 int exponential_search(int *array, size_t size, int value)
 {
+	int index, prev, next;
+
 	if (array == NULL)
-		return (-1);
+		return -1;
 
 	if (array[0] == value)
-		return (0);
+		return 0;
 
-	int index = 1;
+	index = 1;
 
-	while (index < size && array[index] <= value)
+
+
+	while (index < (int)size && array[index] <= value)
 	{
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 		index *= 2;
 	}
 
-	int l_index = index / 2;
-	int r_index = (index < size) ? index : size - 1;
+	prev = index / 2;
+	next = (index < (int)size) ? index : (int)size - 1;
 
-	printf("Value found between indexes [%d] and [%d]\n", l_index, r_index);
+	printf("Value found between indexes [%d] and [%d]\n", prev, next);
 
-	return (binary_search_recursive(array, l_index, r_index, value));
+	return binary_search_recursive(array, prev, next, value);
 }
