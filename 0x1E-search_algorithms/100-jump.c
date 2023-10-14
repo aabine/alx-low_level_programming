ad9566146eb715ceedb7c1791c4c4d3440634bc9
aabine <kendoaustine@yahoo.com>
2023-10-14 13:55:09 +0100

    fixed

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,40 +11,40 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-    int blockSize, prev, step;
+	int blockSize, prev, step;
 
-    if (array == NULL || size == 0)
-        return -1;
+	if (array == NULL || size == 0)
+		return -1;
 
-    blockSize = (int)sqrt((double)size);
-    step = prev = 0;
+	blockSize = (int)sqrt((double)size);
+	step = prev = 0;
 
-    /* Performing jump search across the array */
-    do
-    {
-        printf("Value checked array[%d] = [%d]\n", step, array[step]);
+	/* Performing jump search across the array */
+	do
+	{
+		printf("Value checked array[%d] = [%d]\n", step, array[step]);
 
-        if (array[step] == value)
-            return step;
+		if (array[step] == value)
+			return step;
 
-        prev = step;
-        step += blockSize;
-    } while (step < (int)size && array[step] < value);
+		prev = step;
+		step += blockSize;
+	} while (step < (int)size && array[step] < value);
 
-    printf("Value found between indexes [%d] and [%d]\n", prev, step);
+	printf("Value found between indexes [%d] and [%d]\n", prev, step);
 
-    /* Perform linear search within the range [prev] to [step] */
-    for (; prev <= step && prev < (int)size; prev++)
-    {
-        printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+	/* Perform linear search within the range [prev] to [step] */
+	for (; prev <= step && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 
-        if (array[prev] == value)
-            return prev;
+		if (array[prev] == value)
+			return prev;
 
-        /* Terminate the loop if the target value is exceeded */
-        if (array[prev] > value)
-            break;
-    }
+		/* Terminate the loop if the target value is exceeded */
+		if (array[prev] > value)
+			break;
+	}
 
-    return -1;
+	return -1;
 }
